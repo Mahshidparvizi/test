e6422333e252898bd79e0cff13d4146233d95189
Mahshidparvizi <155556619+Mahshidparvizi@users.noreply.github.com>
2024-05-18 20:29:47 +0330

    Add files via upload

---
+++ New folder (2)/script.js
@@ -0,0 +1,7 @@
+
+let para = document.getElementsByTagName("p");
+document.getElementsByTagName("div").innerHTML = para[1].innerHTML;
+
+let img = document.getElementsByTagName("img");
+let text=img.length;
+document.getElementsByTagName("section")[0].innerHTML = text;
